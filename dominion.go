3dd397f6c4140ae0231db7e3796a2ef59d7b8bad
Oleksii Khilkevych <o.khilkevich@gastrofix.com>
2016-03-05 18:04:23 +0100

    add port check for heroku

--- dominion.go
+++ dominion.go
@@ -104,7 +104,13 @@ func main() {
 			}			
 		})
 
-		log.Fatal(http.ListenAndServe(":5000", nil))
+		port := os.Getenv("PORT")
+
+		if port == "" {
+			port = "5000"
+		}
+
+		log.Fatal(http.ListenAndServe(":" + port, nil))
 	}()
 
     // Ctrl+C handling, doesn't work properly
