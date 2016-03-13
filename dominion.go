a12d6ae2a356f8940d2437da49dc0b77d0dbc430
Oleksii Khilkevych <o.khilkevich@gastrofix.com>
2016-03-13 17:46:13 +0100

    small reformatting

--- dominion.go
+++ dominion.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"time"
-	"strings"
+    "fmt"
+    "log"
+    "os"
+    "time"
+    "strings"
 
-	"github.com/noroutine/bonjour"
-	"github.com/noroutine/dominion/protocol"
-	"github.com/noroutine/dominion/cli"
+    "github.com/noroutine/bonjour"
+    "github.com/noroutine/dominion/protocol"
+    "github.com/noroutine/dominion/cli"
 
-	"github.com/reusee/mmh3"
+    "github.com/reusee/mmh3"
 )
 
 const version = "0.0.7"
@@ -24,7 +24,7 @@ func serviceList() {
     resolver, err := bonjour.NewResolver(nil)
     if err != nil {
         log.Println("Failed to initialize resolver:", err.Error())
-   	    return
+        return
     }
 
     results := make(chan *bonjour.ServiceEntry)
@@ -37,17 +37,17 @@ func serviceList() {
 
 L:
     for {
-    	select {
-    	case e := <- results:
-    		fmt.Printf("%s @ %s (%v)\n", e.Instance, e.HostName, e.AddrIPv4)
-    	case <- time.After(5 * time.Second):
-    		break L
-    	}
+        select {
+        case e := <- results:
+            fmt.Printf("%s @ %s (%v:%d)\n", e.Instance, e.HostName, e.AddrIPv4, e.Port)
+        case <- time.After(1 * time.Second):
+            break L
+        }
     }
 }
 
 func serviceRegister(name string) {
-	// Run registration (blocking call)
+    // Run registration (blocking call)
     _, err := bonjour.Register(name, serviceType, "", servicePort, []string{"txtv=1", "app=test"}, nil)
     if err != nil {
         log.Fatalln(err.Error())
@@ -57,70 +57,65 @@ func serviceRegister(name string) {
 
 func main() {
 
-	port := os.Getenv("PORT")
-	name := "Dominion Player"
+    port := os.Getenv("PORT")
+    name := "Dominion Player"
 
-	if port == "" {
-		port = fmt.Sprintf("%d", servicePort)
-	}
+    if port == "" {
+        port = fmt.Sprintf("%d", servicePort)
+    }
 
-	client := &protocol.Client{ ":" + port, name }
-	go client.Serve()
+    client := &protocol.Client{ ":" + port, name }
+    go client.Serve()
 
-	repl := cli.New()
-	repl.Description = description
-	repl.Prompt = name + "> "
+    repl := cli.New()
+    repl.Description = description
+    repl.Prompt = name + "> "
 
-	go func() {
-		for s := range repl.Signals {
-			if s == os.Interrupt {
-		    	log.Printf("Interrupted")
-				os.Exit(0)
-			}
-		}
-	}()
+    go func() {
+        for s := range repl.Signals {
+            if s == os.Interrupt {
+                log.Printf("Interrupted")
+                os.Exit(0)
+            }
+        }
+    }()
 
-	repl.EmptyHandler = func() {		
-		fmt.Println("Feeling lost? Try 'help'")
-		repl.EmptyHandler = nil
-	}
-	
-	repl.Register("list", func(args []string) {
-		serviceList()
-	})
+    repl.EmptyHandler = func() {        
+        fmt.Println("Feeling lost? Try 'help'")
+        repl.EmptyHandler = nil
+    }
+    
+    repl.Register("list", func(args []string) {
+        serviceList()
+    })
 
-	repl.Register("register", func(args []string) {
-		serviceRegister(name)
-	})
+    repl.Register("register", func(args []string) {
+        serviceRegister(name)
+    })
 
-	repl.Register("help", func(args []string) {
-		fmt.Printf("Commands: %s\n", strings.Join(repl.GetKnownCommands(), ", "))
-	})
+    repl.Register("help", func(args []string) {
+        fmt.Printf("Commands: %s\n", strings.Join(repl.GetKnownCommands(), ", "))
+    })
 
-	repl.Register("sleep", func(args []string) {
-		fmt.Println("Sleeping for 5 seconds...")
-		time.Sleep(5 * time.Second)
-	})
+    repl.Register("mmh3", func(args []string) {
+        key := ""
+        if len(args) > 0 {
+            key = args[0]
+        }
 
-	repl.Register("mmh3", func(args []string) {
-		key := ""
-		if len(args) > 0 {
-			key = args[0]
-		}
+        fmt.Printf("murmur3(\"%s\") = %x\n", key, mmh3.Sum128([]byte(key)))
+    })
 
-		fmt.Printf("murmur3(\"%s\") = %x\n", key, mmh3.Sum128([]byte(key)))
-	})
+    repl.Register("name", func(args []string) {
+        if len(args) > 0 {
+            name = args[0]
+            fmt.Println("You are now", name)
+            repl.Prompt = name + "> "
+            client.PlayerID = name
+        } else {
+            fmt.Println(name)
+        }
+    })
 
-	repl.Register("name", func(args []string) {
-		if len(args) > 0 {
-			name = args[0]
-			fmt.Println("You are now", name)
-			repl.Prompt = name + "> "
-			client.PlayerID = name
-		} else {
-			fmt.Println(name)
-		}
-	})
-
-	repl.Serve()
+    repl.Serve()
 }
